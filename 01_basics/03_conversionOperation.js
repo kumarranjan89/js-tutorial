1120023510109c835baae05288e39918db4341ec
Ranjan Kumar <kumar.ranjan89@gmail.com>
2026-04-07 19:56:33 +0000

    Comment out string conversion examples and operations in conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,10 +30,10 @@
 // null => false
 // undefined => false
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // String Conversion
 // 33 => "33"
@@ -41,4 +41,43 @@ console.log(typeof stringNumber);
 // false => "false"
 // null => "null"
 // undefined => "undefined"
+
+// ################## Operations ##################
+// let value = "33"
+// let negValue = -value
+// // console.log(negValue);
+// console.log(negValue);
+
+// let str1 = "Ranjan"
+// let str2 = " Kumar"
+// console.log(str1 + str2);
+
+// console.log("1" + 2);  // 12
+// console.log(1 + "2");  // 12
+// console.log("1" + 2 + 2); // "122"
+// console.log(1 + "2" + 2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+
+// console.log(+true);  // 33
+// // console.log(true+);  // error
+// console.log(+"")
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 33
+// console.log(num1, num2, num3);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+gameCounter += 1
+console.log(gameCounter);
+
+// 33 => 33
+// "33" => -33
+// "33abc" => NaN
+// true => -1
+// false => -0
+// null => -0
+// undefined => NaN
+
 
