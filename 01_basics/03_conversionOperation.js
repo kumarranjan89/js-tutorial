d1ce11a56baf9f59409b86743cd25e94ca9337d7
Ranjan Kumar <kumar.ranjan89@gmail.com>
2026-04-07 19:36:47 +0000

    Add conversion operation examples in JavaScript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = null  // 33 / 33abc // null / undefined / true / false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// console.log(typeof NaN);
+
+
+// Number Conversion
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = "false"  // 1, 0, null, undefined, "" => false
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// Boolean Conversion
+// 1 => true
+// 0 => false
+// "ranjan" => true
+// "" => false
+// null => false
+// undefined => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// String Conversion
+// 33 => "33"
+// true => "true"
+// false => "false"
+// null => "null"
+// undefined => "undefined"
+
